91c3ab111737704e97fadc4d1a72a66d60a4f298
magdamroczkowska <veriolla7@gmail.com>
2026-06-08 18:05:50 +0200

    Formating source code

--- src/main/java/pl/scoreboard/Team.java
+++ src/main/java/pl/scoreboard/Team.java
@@ -5,19 +5,19 @@ import java.util.stream.Collectors;
 
 public record Team(String name) {
     public Team {
-            name = normalize(name);
-        }
-
-        private static String normalize(String rawName) {
-            if (rawName.isBlank()) {
-                throw new IllegalArgumentException("Team name cannot be blank");
-            }
-
-            return Arrays.stream(rawName.trim().split("\\s+"))
-                    .map(word -> {
-                        String lower = word.toLowerCase();
-                        return Character.toUpperCase(lower.charAt(0)) + lower.substring(1);
-                    })
-                    .collect(Collectors.joining(" "));
-        }
+        name = normalize(name);
     }
+
+    private static String normalize(String rawName) {
+        if (rawName.isBlank()) {
+            throw new IllegalArgumentException("Team name cannot be blank");
+        }
+
+        return Arrays.stream(rawName.trim().split("\\s+"))
+                .map(word -> {
+                    String lower = word.toLowerCase();
+                    return Character.toUpperCase(lower.charAt(0)) + lower.substring(1);
+                })
+                .collect(Collectors.joining(" "));
+    }
+}
